4c4be5800df49f13720df29850178542d99fcc28
Pratiksha-120605 <pratikshad267@gmail.com>
2025-12-20 23:30:48 +0530

    Added ROS2 first node and Dockerfile for Humble workspace

---
+++ ros2_ws/src/my_first_node/my_first_node/my_first_node.py
@@ -0,0 +1,22 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class HelloNode(Node):
+    def __init__(self):
+        super().__init__('hello_node')
+        self.publisher_ = self.create_publisher(String, 'hello_topic', 10)
+        self.timer = self.create_timer(1.0, self.publish_message)
+
+    def publish_message(self):
+        msg = String()
+        msg.data = 'Hello from my first ROS2 node'
+        self.get_logger().info(msg.data)
+        self.publisher_.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = HelloNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
